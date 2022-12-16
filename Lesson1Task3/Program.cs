b3a9b4f86b428720ddaddd3d2973115254257631
Helen <ceemaleon@yandex.ru>
2022-12-16 17:07:38 +0300

    Решение задачи3

---
+++ Lesson1Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine ("Input number");
+int x = Convert.ToInt32(Console.ReadLine());
+if ( x % 2 == 0)
+{
+    Console.Write (x);
+    Console.WriteLine (" - чётное число");
+}
+else
+{
+    Console.Write (x);
+    Console.WriteLine (" - нечётное число");
+}
