80603b83455a2e78a1fc306a545ad311878cb3de
Helen <ceemaleon@yandex.ru>
2022-12-16 17:11:42 +0300

    Решение задачи4

---
+++ Lesson1Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Input number");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < x+1)
+{
+    if ( i % 2 == 0)
+        Console.WriteLine ($"{i}");
+        i= i+1; 
+}
