9c2c81512780001f9650a18439c58ce227c44fd0
Helen <ceemaleon@yandex.ru>
2022-12-16 15:06:36 +0300

    Решение задачи 1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine ("Input first number");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input second number");
+int y = Convert.ToInt32(Console.ReadLine());
+int max = x;
+int min = x;
+if (y > max)
+{
+    max = y; min = x;
+}
+else
+{
+    max = x; min = y;
+}
+Console.Write ("max = ");
+Console.WriteLine (max);
+Console.Write ("min = ");
+Console.WriteLine (min);
