593aa2da4cd3686c22aa9ab6347d195525ad524f
Helen <ceemaleon@yandex.ru>
2022-12-16 17:00:26 +0300

    Решение задачи2

---
+++ Lesson1Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine ("Input first number");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input second number");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input third number");
+int z = Convert.ToInt32(Console.ReadLine());
+int max = x;
+if (y > max)
+{
+    max = y;
+}
+if (z > max)
+{
+    max = z;
+}
+Console.Write ("max = ");
+Console.WriteLine (max);
