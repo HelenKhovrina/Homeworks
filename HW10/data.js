b6d8e7ddcd350bc05c52bad58274e41655ab5cd2
HelenKhovrina <120547369+HelenKhovrina@users.noreply.github.com>
2023-03-11 13:50:53 +0300

    Домашнее задание Семинар10 JS

---
+++ HW10/data.js
@@ -0,0 +1,47 @@
+const datainfo = `[
+    {
+        "id": "ef6",
+        "url": "https://cdn2.thecatapi.com/images/ef6.jpg",
+        "width": 500,
+        "height": 334
+    },
+    {
+        "id": "bpj",
+        "url": "https://cdn2.thecatapi.com/images/bpj.jpg",
+        "width": 796,
+        "height": 1061
+    },
+    {
+        "id": "dt7",
+        "url": "https://cdn2.thecatapi.com/images/dt7.jpg",
+        "width": 765,
+        "height": 1024
+    }
+]`
+
+const Bitcoin = `[
+    {
+    "code": "USD",
+    "symbol": "&#36;",
+    "rate": "19,925.8423",
+    "url": "https://fastly.picsum.photos/id/924/200/300.jpg?hmac=9Zu3ewQYhI2ltbuwGQk-Ed6PjR87O-zdiPty45pJS6g",
+    "description": "United States Dollar",
+    "rate_float": 19925.8423
+  },
+    {
+    "code": "GBP",
+    "symbol": "&pound;",
+    "rate": "16,649.8744",
+    "url": "https://fastly.picsum.photos/id/456/200/300.jpg?hmac=p9jAgKWZ8BmAcmdpyO2siEEUZaIzEUHYN64WS2rJNtM",
+    "description": "British Pound Sterling",
+    "rate_float": 16649.8744
+  },
+    {
+    "code": "EUR",
+    "symbol": "&euro;",
+    "rate": "19,410.6796",
+    "url": "https://fastly.picsum.photos/id/482/200/300.jpg?hmac=sZqH9D718kRNYORntdoWP-EehCC83NaK3M-KTWvABIg",
+    "description": "Euro",
+    "rate_float": 19410.6796
+  }
+]`
